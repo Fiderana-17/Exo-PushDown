f5217ae2cb4c729b44d99485e6403445b09c23fd
Fiderana-17 <hei.fiderana@gmail.com>
2026-02-18 16:16:14 +0300

    feat: Calcul the total amount of each invoice

---
+++ src/main/java/org/example/Ressources/Requetes.sql
@@ -0,0 +1,8 @@
+SELECT
+    i.id,
+    i.customer_name,
+    SUM(il.quantity * il.unit_price) AS total
+FROM invoice i
+         JOIN invoice_line il ON il.invoice_id = i.id
+GROUP BY i.id, i.customer_name
+ORDER BY i.id;
