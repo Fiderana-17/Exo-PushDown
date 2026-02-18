4ee4fa198c3db2863ed8c9d1eb157a6124bba489
Fiderana-17 <hei.fiderana@gmail.com>
2026-02-18 16:17:27 +0300

    feat: Display the total per invoice only for invoices with the status

--- src/main/java/org/example/Ressources/Requetes.sql
+++ src/main/java/org/example/Ressources/Requetes.sql
@@ -6,3 +6,15 @@ FROM invoice i
          JOIN invoice_line il ON il.invoice_id = i.id
 GROUP BY i.id, i.customer_name
 ORDER BY i.id;
+
+
+SELECT
+    i.id,
+    i.customer_name,
+    i.status,
+    SUM(il.quantity * il.unit_price) AS total
+FROM invoice i
+         JOIN invoice_line il ON il.invoice_id = i.id
+WHERE i.status IN ('CONFIRMED', 'PAID')
+GROUP BY i.id, i.customer_name, i.status
+ORDER BY i.id;
